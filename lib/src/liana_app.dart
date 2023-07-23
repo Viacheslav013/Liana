39213be8ccd00dc8c8c4c20b5d78f196c3d96805
Viacheslav013 <viacheslav.dev.013@gmail.com>
2023-07-23 15:06:14 +0300

    Create and setup empty project

---
+++ lib/src/liana_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class LianaApp extends StatelessWidget {
+  const LianaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Implement later
+    throw UnimplementedError();
+  }
+}
---
+++ scripts/run_code_generate.sh
@@ -0,0 +1,4 @@
+set -e
+
+flutter pub get
+flutter packages pub run build_runner build --delete-conflicting-outputs
