45962d2a4d7da08979adeca12e3d5a487cba7d17
Viacheslav013 <viacheslav.dev.013@gmail.com>
2023-08-24 17:15:57 +0300

    Implement splash screen, add app icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:liana/src/di/di_widget.dart';
 import 'package:liana/src/liana_app.dart';
 import 'package:liana/src/presentation/localization/localization_widget.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
+void main() {
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   runApp(
     const DiWidget(
       child: LocalizationWidget(
+        onInitialized: FlutterNativeSplash.remove,
         child: LianaApp(),
       ),
     ),
